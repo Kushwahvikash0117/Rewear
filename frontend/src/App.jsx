d2f57de5adac7cb135dde1da017f5ea88899aec9
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-07-12 16:52:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,34 @@
 import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProductDesign from './pages/ProductDesign';
-
+import HomeLanding from "./pages/HomeLanding.jsx";
+import MyListings from "./pages/MyListings.jsx";
+import SwapRequests from "./pages/SwapRequests.jsx";
+import Loading from './pages/Loading.jsx';
+import { Navbar } from './components/Navbar.jsx';
+import AdminPanel from './pages/AdminPanel.jsx';
 function App() {
+  
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        
-        <Route path="/product-design" element={<ProductDesign />} />
-
-      </Routes>
-    </Router>
+    <>
+    
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={HomeLanding}></Route>
+          <Route path='/swap-requests' element={SwapRequests}></Route>
+          <Route path='/my-listings' element={MyListings}></Route>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/product-design" element={<ProductDesign />} />
+          <Route path='/admin-panel' element={AdminPanel}></Route>
+          <Route path='/loading' element={Loading}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
