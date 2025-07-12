fb1ffa76346970d3797b9a8061cef281f756d7ac
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-07-12 17:01:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ import swapRoutes from './routes/swapRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
+import loadEnv from './config/dotenvConfig.js';
+
+
+loadEnv();
+
 
 dotenv.config();
 connectDB();
@@ -15,6 +20,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
+
+
+// enable file uploads with temp files
+app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/items', itemRoutes);
