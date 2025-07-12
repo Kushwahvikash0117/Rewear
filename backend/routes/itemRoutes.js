62120e0a92e5928826f0cb63d8af5bd4c7646ccd
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-07-12 16:59:28 +0530

    Update itemRoutes.js

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { uploadItem, getItems } from '../controllers/itemController.js';
 import { protect } from '../middlewares/authMiddleware.js';
+import upload from '../utils/multer.js';
 
 const router = express.Router();
-router.post('/upload', protect, uploadItem);
+
+router.post('/upload', protect, upload.array('images', 3), uploadItem);
 router.get('/', getItems);
 
 export default router;
