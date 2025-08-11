e7b874a928cc7a3d81ad1e05f36aa50ba4a360da
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-08-11 22:59:13 +0530

    Update loading.jsx

--- frontend/src/pages/loading.jsx
+++ frontend/src/pages/loading.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import './Loading.css';
 
 const Loading = () => {
   return (
-    <div className="loading-container">
-      <div className="spinner"></div>
-      <p>Loading, please wait...</p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      {/* Spinner */}
+      <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin mb-6"></div>
+
+      {/* Loading Text */}
+      <p className="text-lg text-gray-700">Loading, please wait...</p>
     </div>
   );
 };
