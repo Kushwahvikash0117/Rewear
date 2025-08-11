3664e667eef4fc8553c784971d6400f1e5158ada
Vikash Kushwah <vikash.vk.0117@gmail.com>
2025-08-11 22:54:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,34 @@
-import './App.css'
+// App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useLocation, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomeLanding from './pages/HomeLanding';
+import MyListings from './pages/MyListings';
+import SwapRequests from './pages/SwapRequests';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import ProductDesign from './pages/ProductDesign';
-import HomeLanding from "./pages/HomeLanding.jsx";
-import MyListings from "./pages/MyListings.jsx";
-import SwapRequests from "./pages/SwapRequests.jsx";
-import Loading from './pages/Loading.jsx';
-import { Navbar } from './components/Navbar.jsx';
-import AdminPanel from './pages/AdminPanel.jsx';
+import AdminPanel from './pages/AdminPanel';
+import Loading from './pages/Loading';
+
 function App() {
-  
+  const location = useLocation();
+  const hideNavbarRoutes = ['/login', '/signup'];
+  const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname);
+
   return (
     <>
-    
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={HomeLanding}></Route>
-          <Route path='/swap-requests' element={SwapRequests}></Route>
-          <Route path='/my-listings' element={MyListings}></Route>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/product-design" element={<ProductDesign />} />
-          <Route path='/admin-panel' element={AdminPanel}></Route>
-          <Route path='/loading' element={Loading}></Route>
-        </Routes>
-      </Router>
+      {shouldShowNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<HomeLanding />} />
+        <Route path="/swap-requests" element={<SwapRequests />} />
+        <Route path="/my-listings" element={<MyListings />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/product-design" element={<ProductDesign />} />
+        <Route path="/admin-panel" element={<AdminPanel />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
     </>
   );
 }
